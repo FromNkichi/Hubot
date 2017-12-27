f8cc6944fd53b4a79426893b2d12bc37b45f108d
FromNkichi <ee57031@meiji.ac.jp>
2017-12-28 02:15:48 +0900

    monitの監視システムに対応

--- scripts/initialize.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    'use strict';
-    const exec = require('child_process').exec;
-    const file = require('fs');
-    let hubotProcessID = 0;
-    exec('ps aux | grep hubot | grep slack', (error, stdout, stderr) => {
-        if (stdout) {
-            const ps = stdout.split(" ");
-            hubotProcessID = ps[1];
