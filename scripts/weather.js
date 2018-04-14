e0dbd78beb35624100b9063182c0c58a7177352a
FromNkichi <ee57031@meiji.ac.jp>
2018-04-14 22:58:12 +0900

    OpenWeatherAPIに対応

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,129 +1,50 @@
-const twitter = require('twitter');
 const fs = require('fs');
+const http = require('http');
+const weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?';
 const path = require('path');
 const folder = path.resolve('./');
-const tokenFilePath = folder + '/scripts/keys/';
-const pictureFilePath = folder + '/scripts/bashes/';
-const Gyazo = require('gyazo-api');
-const exec = require('child_process').exec;
-const noon = 12;
-const sunday = 7;
+const userFilePath = folder + '/scripts/keys/openweathermap/';
+const file = 'api.key';
+
+
 
 module.exports = (hubot) => {
     hubot.hear('weather', (res) => {
-        function myReadFile(file) {
+        function myReadFile(filepwd) {
             return new Promise((resolve, reject) => {
-                fs.readFile(tokenFilePath + file, 'utf8', (err, text) => {
+                fs.readFile(filepwd, 'utf8', (err, text) => {
                     const result = text.replace('\n', '');
                     resolve(result);
                 });
             });
         }
 
-        let twitterResist = function(result) {
+        let getWeather = function(result) {
             return new Promise((resolve, reject) => {
-                let twi = new twitter({
-                    consumer_key: result[0],
-                    consumer_secret: result[1],
-                    access_token_key: result[2],
-                    access_token_secret: result[3]
-                });
-                resolve(twi);
-            });
-        }
+                const url = weatherUrl + 'q=Yokohama-shi,jp' + '&units=metric' + '&appid=' + result;
+                console.log(url);
 
-
-        let tweet = function(twi) {
-            return new Promise((resolve, reject) => {
-                twi.get('search/tweets', {q:'from:dmenu_tenki'}, (err, tweets, res) => {
-                    if (!err) {
-                        resolve(tweets.statuses[0].text);
-                    } else {
-                        console.log(twi);
-                        console.log(err);
-                    }
-                });
-            });
-        }
-
-        let msgEdit = function(msg) {
-            return new Promise((resolve, reject) => {
-                let ret = msg.split('#');
-                resolve(ret[0]);
-            });
-        }
-
-        let response = function(msg) {
-            return new Promise((resolve, reject) => {
-                hubot.messageRoom('C8NFCKM39', msg);
-                resolve();
-            });
-        }
-
-        let gyazoResist = function(result) {
-            return new Promise((resolve, reject) => {
-                let gyazo = new Gyazo(result[0]);
-                resolve(gyazo);
-            });
-        }
-
-        let makePic = function(obj) {
-            const date = new Date();
-            let command = pictureFilePath + 'weather_image ';
-            return new Promise((resolve, reject) => {
-                if (date.getDay() == sunday && date.getHours() < noon) {
-                    command += 'week';
-                } else if (date.getHours() < noon) {
-                    command += 'today';
-                } else {
-                    command += 'tomorrow';
-                }
-                exec(command, (error, stdout, stderr)=> {
-                    console.log('hoge');
-                    resolve(obj);
-                });
-            });
-
-        }
-
-        let pushGyazo = function(gyazo) {
-            const date = new Date();
-            let picturePath = pictureFilePath;
-            return new Promise((resolve, reject) => {
-                if (date.getDay() == sunday && date.getHours() < noon) {
-                    picturePath += 'week.png';
-                } else if (date.getHours() < noon) {
-                    picturePath += 'today.png';
-                } else {
-                    picturePath += 'tomorrow.png';
-                }
-                gyazo.upload(picturePath, {
-                    title: 'today',
-                    desc : 'upload from nodejs'
-                    })
-                    .then(function(res){
-                        resolve(res.data.permalink_url);
-                    })
-                    .catch(function(err){
-                        console.log(err);
+                http.get(url, (res) => {
+                    let body = '';
+                    res.setEncoding('utf8');
+        
+                    res.on('data', (chunk) => {
+                        body += chunk;
                     });
-            });
+        
+                    res.on('data', (chunk) => {
+                        result = JSON.parse(body);
+                        console.log(result);
+                    });
+                }).on('error', (e) => {
+                    console.log(e.message);
+                });
+                        
+            })
         }
+        let weatherPromise = Promise.all([myReadFile(userFilePath + file)]);
 
-        let twitterPromise = Promise.all([myReadFile('twitter/consumer.key'), myReadFile('twitter/consumer_secret.key'), myReadFile('twitter/access.token'), myReadFile('twitter/access_secret.token')]);
-
-        let gyazoPromise = Promise.all([myReadFile('gyazo/gyazo.token')]);
-
-        twitterPromise
-            .then(twitterResist)
-            .then(tweet)
-            .then(msgEdit)
-            .then(response);
-
-        gyazoPromise
-            .then(makePic)
-            .then(gyazoResist)
-            .then(pushGyazo)
-            .then(response);
+        weatherPromise
+            .then(getWeather);
     });
 }
